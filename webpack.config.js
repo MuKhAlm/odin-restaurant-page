c6d7b4e42a114e98213226fda666fe7bf0759273
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-10-09 13:09:51 +0100

    Apply style-loader and css-loader to css files

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,17 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
